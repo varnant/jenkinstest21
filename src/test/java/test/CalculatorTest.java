2a25bbf809b9f6253115116cccfc6854b03900f8
varnan <varnant@gmail.com>
2026-06-20 18:58:03 +0530

    2nd commit

--- src/test/java/test/CalculatorTest.java
+++ src/test/java/test/CalculatorTest.java
@@ -9,7 +9,7 @@ class CalculatorTest {
     void testAddition() {
         Calculator calc = new Calculator();
         int result = calc.add(10, 20);
-
+        System.out.println("result is "+result);
         assertEquals(30, result);//here we cant use Lambda as assertEquals looking for value not behaviour(method call)
     }
     
