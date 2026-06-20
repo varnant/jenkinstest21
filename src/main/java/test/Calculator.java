dc044cd0dcd92ec18116bb67dcbfa24248bc6c57
varnan <varnant@gmail.com>
2026-06-20 18:33:31 +0530

    first commit

---
+++ src/main/java/test/Calculator.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int div(int a, int b) {
+        return a / b;
+    }
+}
+//f6 to continue f5 to step into a method call
---
+++ src/test/java/test/CalculatorTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calc = new Calculator();
+        int result = calc.add(10, 20);
+
+        assertEquals(30, result);//here we cant use Lambda as assertEquals looking for value not behaviour(method call)
+    }
+    
+}
+/*AAA pattern
+ * Arrange :set up the objects variables and conditions needed for the test
+ * Act:Invoke the specific method(behaviour)
+ * Assert:compare the actual result with the expected result
+ */
